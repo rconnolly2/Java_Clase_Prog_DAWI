016ec4ec75e9a259f8b99a1f55f7dddb3bb55dc0
Robert Connolly <robertoconnolly100@gmail.com>
2023-11-27 12:33:04 +0100

    actividad2

--- Actividades_4_voluntaria_prog/unidad4.java
+++ Actividades_4_voluntaria_prog/unidad4.java
@@ -16,11 +16,15 @@ public class unidad4 {
         System.out.println("La superficie es: " + (4*Math.PI*(Math.pow(radio, 2))));
         System.out.println("Y el volumen es: " + ((4*Math.PI)/3)*(Math.pow(radio, 3)));
     }
+
+    public static double actividad2(int x1, int y1, int x2, int y2) {
+        return Math.sqrt(Math.pow((x1-x2), 2)+Math.pow((y1-y2), 2)); // return distancia
+    }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Dame el radio: ");
-        actividad1(sc.nextInt());
+        System.out.println("Dame los puntos x1, y1, x2, y2: ");
+        System.out.println("La distancia es: " + actividad2(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt()));
       
         System.exit(0); // fin del programa
     }
